f17a1160b9f91502999f8bb4e8f92a26a96dd955
Ashish-Prakash <ashishprakash52433@gmail.com>
2022-10-20 21:35:34 +0530

    push

---
+++ DLL.java
@@ -0,0 +1,382 @@
+import java.util.Scanner;
+
+public class DLL {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class DoublyLinkedList {
+        private class Node {
+            int val;
+            Node prev;
+            Node next;
+
+            Node(int val) {
+                this.val = val;
+                this.prev = null;
+                this.next = null;
+            }
+        }
+
+        private Node head = null;
+        private Node tail = null;
+        private int size = 0;
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            Node curr = head;
+            sb.append("[");
+            while (curr != null) {
+                sb.append(curr.val);
+                if (curr.next != null) {
+                    sb.append(", ");
+                }
+                curr = curr.next;
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+
+        public void displayForw() {
+            StringBuilder sb = new StringBuilder();
+            Node curr = this.head;
+            sb.append("[");
+            while (curr != null) {
+                sb.append(curr.val);
+                if (curr.next != null) {
+                    sb.append(", ");
+                }
+                curr = curr.next;
+            }
+            sb.append("]");
+            System.out.println(sb.toString());
+        }
+
+        public void displayBack() {
+            StringBuilder sb = new StringBuilder();
+            Node curr = this.tail;
+            sb.append("[");
+            while (curr != null) {
+                sb.append(curr.val);
+                if (curr.prev != null) {
+                    sb.append(", ");
+                }
+                curr = curr.prev;
+            }
+            sb.append("]");
+            System.out.println(sb.toString());
+        }
+
+        public int size() {
+            return this.size;
+        }
+
+        public boolean isEmpty() {
+            return this.size == 0;
+        }
+
+        private boolean ListIsEmptyException() {
+            if (this.size == 0) {
+                System.out.print("ListIsEmpty: ");
+                return true;
+            }
+            return false;
+        }
+
+        private boolean indexIsInvalidException(int index, int leftRange, int rightRange) {
+            if (index < leftRange || index > rightRange) {
+                System.out.print("IndexIsInValid: ");
+                return true;
+            }
+            return false;
+        }
+
+        private void addFirstNode(Node node) {
+            if (this.size == 0) {
+                this.head = node;
+                this.tail = node;
+            } else {
+                node.next = this.head;
+                this.head.prev = node;
+                this.head = node;
+            }
+            this.size++;
+        }
+
+        public void addFirst(int val) {
+            Node node = new Node(val);
+            addFirstNode(node);
+        }
+
+        private void addLastNode(Node node) {
+            if (this.size == 0) {
+                this.head = this.tail = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+                this.tail = node;
+            }
+            this.size--;
+        }
+
+        public void addLast(int val) {
+            Node node = new Node(val);
+            addLastNode(node);
+        }
+
+        public void addNodeAt(int idx, Node node) {
+            if (idx == 0) {
+                addFirstNode(node);
+            } else if (idx == this.size) {
+                addLastNode(node);
+            } else {
+                Node forw = getNodeAt(idx);
+                Node prev = forw.prev;
+
+                prev.next = node;
+                node.prev = prev;
+
+                node.next = forw;
+                forw.prev = node;
+                this.size++;
+            }
+        }
+
+        public void addAt(int index, int data) {
+            if (indexIsInvalidException(index, 0, this.size)) {
+                System.out.println(-1);
+                return;
+            } else {
+                Node node = new Node(data);
+                addNodeAt(index, node);
+            }
+        }
+
+        public void addBefore(Node refNode, int data) {
+            Node node = new Node(data);
+            Node prev = refNode.prev;
+            if (prev == null) {
+                refNode.prev = node;
+                node.next = refNode;
+                this.head = node;
+            } else {
+                prev.next = node;
+                node.prev = prev;
+                node.next = refNode;
+                refNode.prev = node;
+            }
+            this.size++;
+        }
+
+        public void addBefore(int idx, int data) {
+            Node node = getNodeAt(idx);
+            addBefore(node, data);
+        }
+
+        public void addAfter(Node refNode, int data) {
+            Node node = new Node(data);
+            Node next = refNode.next;
+            if (next == null) {
+                refNode.next = node;
+                node.prev = refNode;
+                this.tail = node;
+            } else {
+                refNode.next = node;
+                node.prev = refNode;
+                node.next = next;
+                next.prev = node;
+            }
+            this.size++;
+        }
+
+        public void addAfter(int idx, int data) {
+            Node node = getNodeAt(idx);
+            addAfter(node, data);
+        }
+
+        private Node removeFirstNode() {
+            Node node = this.head;
+            if (this.size == 1) {
+                this.head = this.tail = null;
+            } else {
+                Node forw = this.head.next;
+                forw.prev = null;
+                node.next = null;
+                this.head = forw;
+            }
+            this.size--;
+            return node;
+        }
+
+        public int removeFirst() {
+            if (ListIsEmptyException()) {
+                return -1;
+            }
+            return removeFirstNode().val;
+        }
+
+        private Node removeLastNode() {
+            Node node = this.tail;
+            if (this.size == 1) {
+                this.head = this.tail = null;
+            } else {
+                Node slast = this.tail.prev;
+                slast.next = null;
+                this.tail.prev = null;
+                this.tail = slast;
+            }
+            this.size--;
+            return node;
+        }
+
+        public int removeLast() {
+            if (ListIsEmptyException()) {
+                return -1;
+            }
+            return removeLastNode().val;
+        }
+
+        public Node removeAtNode(int idx) {
+            if (idx == 0) {
+                return removeFirstNode();
+            }
+            if (idx == this.size - 1) {
+                return removeLastNode();
+            } else {
+                Node node = getNodeAt(idx);
+                Node forw = node.next;
+                Node prev = node.prev;
+
+                prev.next = forw;
+                forw.prev = prev;
+
+                node.next = null;
+                node.prev = null;
+                this.size--;
+                return node;
+            }
+        }
+
+        public int removeAt(int index) {
+            if (ListIsEmptyException()) {
+                return -1;
+            } else if (indexIsInvalidException(index, 0, this.size - 1)) {
+                return -1;
+            }
+            return removeAtNode(index).val;
+        }
+
+        public Node removeAfterNode(Node refNode) {
+            Node forw = refNode.next;
+            if (forw.next == null) {
+                refNode.next = null;
+                forw.prev = null;
+                this.tail = refNode;
+            } else {
+                Node next = forw.next;
+                refNode.next = next;
+                next.prev = refNode;
+                forw.prev = null;
+                forw.next = null;
+            }
+            this.size--;
+            return forw;
+        }
+
+        public int removeAfter(Node refNode) {
+            if (refNode.next == null) {
+                System.out.print("LocationIsInvalid: ");
+                return -1;
+            } else {
+                return removeAfterNode(refNode).val;
+            }
+        }
+
+        public int removeAfter(int idx) {
+            Node node = getNodeAt(idx);
+            return removeAfter(node);
+        }
+
+        public Node removeBeforeNode(Node refNode) {
+            Node prev = refNode.prev;
+            if (prev.prev == null) {
+                prev.next = null;
+                refNode.prev = null;
+                this.head = refNode;
+            } else {
+                Node pprev = prev.prev;
+                pprev.next = refNode;
+                refNode.prev = pprev;
+                prev.next = null;
+                prev.prev = null;
+            }
+            this.size--;
+            return prev;
+        }
+
+        public int removeBefore(Node refNode) {
+            if (refNode.prev == null) {
+                System.out.print("LocationIsInvalid: ");
+                return -1;
+            } else {
+                return removeBeforeNode(refNode).val;
+            }
+        }
+
+        public int removeBefore(int idx) {
+            Node node = getNodeAt(idx);
+            return removeBefore(node);
+        }
+
+        public int removeNode(Node refNode) {
+            Node prev = refNode.prev, next = refNode.next;
+            if (this.size == 1) {
+                this.head = this.tail = null;
+            } else if (prev == null) {
+                this.head = next;
+            } else if (next == null) {
+                this.tail = prev;
+            } else {
+                prev.next = next;
+                next.prev = prev;
+            }
+            refNode.next = refNode.prev = this.head.prev = this.tail.next = null;
+            this.size--;
+            return refNode.val;
+        }
+
+        public int getFirst() {
+            if (ListIsEmptyException()) {
+                return -1;
+            }
+            return this.head.val;
+        }
+
+        public int getLast() {
+            if (ListIsEmptyException()) {
+                return -1;
+            }
+            return this.tail.val;
+        }
+
+        public Node getNodeAt(int idx) {
+            Node curr = head;
+            while (idx-- > 0) {
+                curr = curr.next;
+            }
+            return curr;
+        }
+
+        public int getAt(int index) {
+            if (ListIsEmptyException()) {
+                return -1;
+            } else if (indexIsInvalidException(index, 0, this.size - 1)) {
+                return -1;
+            } else {
+                Node node = getNodeAt(index);
+                return node.val;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ LL.java
@@ -0,0 +1,506 @@
+import java.util.Scanner;
+
+public class LL {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode reverse_rec(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode node = reverse_rec(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode forw = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
+
+    public static ListNode midNode(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+
+        ListNode mid = midNode(head);
+        ListNode temp = mid.next;
+        temp = reverse(temp);
+        mid.next = null;
+        ListNode p1 = head, p2 = temp;
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        temp = reverse(temp);
+        mid.next = temp;
+        return true;
+    }
+
+    public static void fold(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+        ListNode mid = midNode(head);
+        ListNode temp = mid.next;
+        temp = reverse(temp);
+        mid.next = null;
+        ListNode p1 = head, p2 = temp;
+        while (p2 != null) {
+            ListNode h1 = p1.next, h2 = p2.next;
+            p1.next = p2;
+            p2.next = h1;
+
+            p1 = h1;
+            p2 = h2;
+        }
+    }
+
+    public static void unfold(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+        ListNode p1 = head, p2 = head.next;
+        ListNode h1 = p1, h2 = p2;
+        while (h2 != null && h2.next != null) {
+            ListNode next = h2.next;
+            h1.next = next;
+            h2.next = next.next;
+
+            h1 = h1.next;
+            h2 = h2.next;
+        }
+        p2 = reverse(p2);
+        h1.next = p2;
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy, p1 = l1, p2 = l2;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                temp.next = new ListNode(p1.val);
+                p1 = p1.next;
+            } else {
+                temp.next = new ListNode(p2.val);
+                p2 = p2.next;
+            }
+            temp = temp.next;
+        }
+        while (p1 != null) {
+            temp.next = new ListNode(p1.val);
+            p1 = p1.next;
+            temp = temp.next;
+        }
+        while (p2 != null) {
+            temp.next = new ListNode(p2.val);
+            p2 = p2.next;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+
+    public static ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = midNode(head);
+        ListNode next = mid.next;
+        mid.next = null;
+
+        return mergeTwoLists(mergeSort(head), mergeSort(next));
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0)
+            return null;
+        return mergeKLists(lists, 0, lists.length - 1);
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists, int s, int e) {
+        if (s == e) {
+            return lists[s];
+        }
+        int mid = (s + e) / 2;
+        ListNode left = mergeKLists(lists, s, mid);
+        ListNode right = mergeKLists(lists, mid + 1, e);
+        return mergeTwoLists(left, right);
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null)
+            return head;
+        ListNode fast = head, slow = head;
+        while (n-- > 0) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        ListNode rem = slow.next;
+        slow.next = rem.next;
+        rem.next = null;
+        return head;
+    }
+
+    public static ListNode th = null, tt = null;
+
+    public static int length(ListNode head) {
+        if (head == null)
+            return 0;
+        ListNode curr = head;
+        int len = 0;
+        while (curr != null) {
+            curr = curr.next;
+            len++;
+        }
+        return len;
+    }
+
+    public static void addFirstNode(ListNode head) {
+        if (th == null) {
+            th = head;
+            tt = head;
+        } else {
+            head.next = th;
+            th = head;
+        }
+    }
+
+    public static ListNode reverseInKGroup(ListNode head, int k) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode curr = head, oh = null, ot = null;
+        int len = length(head);
+        while (len >= k) {
+            int tempK = k;
+            while (curr != null && tempK-- > 0) {
+                ListNode forw = curr.next;
+                curr.next = null;
+                addFirstNode(curr);
+                curr = forw;
+            }
+            if (oh == null) {
+                oh = th;
+                ot = tt;
+            } else {
+                ot.next = th;
+                ot = tt;
+            }
+            th = tt = null;
+            len -= k;
+        }
+        ot.next = curr;
+        return oh;
+    }
+
+    public static ListNode reverseInRange(ListNode head, int n, int m) {
+        if (head == null || head.next == null)
+            return head;
+
+        return null;
+    }
+
+    public static ListNode removeDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode curr = head.next, prev = head;
+        while (curr != null) {
+            while (curr != null && curr.val == prev.val) {
+                ListNode forw = curr.next;
+                curr.next = null;
+                curr = forw;
+            }
+            prev.next = curr;
+            prev = prev.next;
+            if (curr != null) {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+
+    public static ListNode removeAllDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode dummy = new ListNode(-1);
+        ListNode point = dummy;
+        point.next = head;
+        ListNode curr = head.next;
+        while (curr != null) {
+            boolean flag = false;
+            while (curr != null && curr.val == point.next.val) {
+                flag = true;
+                curr = curr.next;
+            }
+            if (flag) {
+                point.next = curr;
+            } else {
+                point = point.next;
+            }
+            if (curr != null) {
+                curr = curr.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        int carry = 0;
+        ListNode dummy = new ListNode(-1);
+        ListNode itr = dummy;
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            itr.next = new ListNode(sum);
+            itr = itr.next;
+        }
+        return reverse(dummy.next);
+    }
+
+    public static int getLength(ListNode head) {
+        if (head == null)
+            return 0;
+        ListNode curr = head;
+        int len = 0;
+        while (curr != null) {
+            len++;
+            curr = curr.next;
+        }
+        return len;
+    }
+
+    public static boolean isBigger(ListNode l1, ListNode l2) {
+        while (l1 != null) {
+            if (l1.val != 0) {
+                break;
+            }
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            if (l2.val != 0) {
+                break;
+            }
+            l2 = l2.next;
+        }
+        int len1 = getLength(l1);
+        int len2 = getLength(l2);
+        if (len1 > len2) {
+            return true;
+        } else if (len1 < len2) {
+            return false;
+        }
+        ListNode c1 = l1, c2 = l2;
+        while (c1 != null) {
+            if (c1.val > c2.val)
+                return true;
+            else if (c1.val < c2.val)
+                return false;
+            c1 = c1.next;
+            c2 = c2.next;
+        }
+        return true;
+    }
+
+    public static ListNode subtractTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        ListNode c1 = null, c2 = null;
+        if (isBigger(l1, l2)) {
+            c1 = reverse(l1);
+            c2 = reverse(l2);
+        } else {
+            c1 = reverse(l2);
+            c2 = reverse(l1);
+        }
+        ListNode dummy = new ListNode(-1), itr = dummy;
+        int borrow = 0;
+        while (c1 != null || c2 != null) {
+            int diff = borrow;
+            if (c1 != null) {
+                diff += c1.val;
+                c1 = c1.next;
+            }
+            if (c2 != null) {
+                diff -= c2.val;
+                c2 = c2.next;
+            }
+            if (diff < 0) {
+                borrow = -1;
+                diff += 10;
+            } else {
+                borrow = 0;
+            }
+            itr.next = new ListNode(diff);
+            itr = itr.next;
+        }
+        ListNode base = reverse(dummy.next), prev = dummy, curr = base;
+        prev.next = null;
+        while (curr != null) {
+            if (curr.val != 0) {
+                prev.next = curr;
+                break;
+            }
+            ListNode forw = curr.next;
+            curr.next = null;
+            curr = forw;
+        }
+        return dummy.next == null ? new ListNode(0) : dummy.next;
+    }
+
+    public static ListNode multiplyTwoLL(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    public static ListNode copyRandomList(ListNode head) {
+        return null;
+    }
+
+    public static ListNode segregateEvenOdd(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode even = new ListNode(-1), odd = new ListNode(-1);
+        ListNode p1 = even, p2 = odd;
+        ListNode curr = head;
+        while (curr != null) {
+            if ((curr.val & 1) == 0) {
+                p1.next = curr;
+                p1 = p1.next;
+            } else {
+                p2.next = curr;
+                p2 = p2.next;
+            }
+            curr = curr.next;
+        }
+        p1.next = odd.next;
+        p2.next = null;
+        return even.next;
+    }
+
+    public static ListNode segregate01(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode zero = new ListNode(-1), one = new ListNode(-1);
+        ListNode p1 = zero, p2 = one, curr = head;
+        while (curr != null) {
+            if (curr.val == 0) {
+                p1.next = curr;
+                p1 = p1.next;
+            } else {
+                p2.next = curr;
+                p2 = p2.next;
+            }
+            curr = curr.next;
+        }
+        p1.next = one.next;
+        p2.next = null;
+        return zero.next;
+    }
+
+    public static ListNode segregate012(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode zero = new ListNode(-1), one = new ListNode(-1), two = new ListNode(-1);
+        ListNode p1 = zero, p2 = one, p3 = two;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.val == 0) {
+                p1.next = curr;
+                p1 = p1.next;
+            } else if (curr.val == 1) {
+                p2.next = curr;
+                p2 = p2.next;
+            } else {
+                p3.next = curr;
+                p3 = p3.next;
+            }
+            curr = curr.next;
+        }
+        p2.next = two.next;
+        p1.next = one.next;
+        p3.next = null;
+        return zero.next;
+    }
+
+    public static void display(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode makeList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while (n-- > 0) {
+            int val = scn.nextInt();
+            temp.next = new ListNode(val);
+            temp = temp.next;
+        }
+        ListNode head = dummy.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = makeList(scn.nextInt());
+        display(head);
+    }
+}
